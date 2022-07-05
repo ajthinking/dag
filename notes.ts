a36793e306aaa9bfdaa6adf474ff737617d34365
Anders Jurisoo <jurisoo@hotmail.com>
2022-07-05 10:28:41 +0200

    wip

---
+++ notes.ts
@@ -0,0 +1,7 @@
+// the main diagram is started
+// it finds all starters
+// it starts all starters one at a time
+// a create node outputs a new item
+// the item is stored at a link
+// the diagram notifies the node that new items are available (onNewItems)
+// the node runs
